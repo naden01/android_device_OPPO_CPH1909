37ce6b3322b0d972f7e6aae52ab3a1c18161a663
YFMARCO <147658205+YFMARCO@users.noreply.github.com>
2024-10-10 22:30:07 +0300

    Update omni_CPH1909.mk

--- omni_CPH1909.mk
+++ omni_CPH1909.mk
@@ -1,24 +1,19 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from CPH1909 device
-$(call inherit-product, device/OPPO/CPH1909/device.mk)
-
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := CPH1909
 PRODUCT_NAME := omni_CPH1909
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := CPH1909
 PRODUCT_MANUFACTURER := OPPO
-
-PRODUCT_GMS_CLIENTID_BASE := android-oppo

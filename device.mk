365ddde2552f9865e4cf9661af0bdf5d6edbe14c
ZG_DEV <159061718+ZG089@users.noreply.github.com>
2024-10-23 03:10:40 +0300

    Attempt to fix mtp

--- device.mk
+++ device.mk
@@ -13,3 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 LOCAL_PATH := device/oppo/CPH1909
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
